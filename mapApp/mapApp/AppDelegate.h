ea2d19dfc22cde6535958f9ad2db69ea5d4d2c81
Derek Neil <derekrneil@gmail.com>
2013-10-30 22:56:50 -0300

    weird thing when i tried to move the project to another folder

---
+++ mapApp/mapApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mapApp
+//
+//  Created by Derek Neil on 2013-10-30.
+//  Copyright (c) 2013 ship-fit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mapApp/mapApp/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  mapApp
+//
+//  Created by Derek Neil on 2013-10-30.
+//  Copyright (c) 2013 ship-fit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
