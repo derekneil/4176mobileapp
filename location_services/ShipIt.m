3c4edcaf9f710cd96d8a51648a41ef148d72273c
jammin <jammin.irving@gmail.com>
2013-10-30 21:54:21 -0300

    Bare Bones, bugs, but check out the properties in ShipIt.h, that is whats important for you

---
+++ location_services/ShipIt.m
@@ -0,0 +1,35 @@
+//
+// ShipIt.m
+// Copyright (c) 2013 BenJammin Irving on 10/29/13.
+// All rights reserved.
+//
+
+#import "ShipIt.h"
+
+
+// Compass Bearing 
+NSString *const N = @"N";
+NSString *const NW = @"NW";
+NSString *const W = @"W";
+NSString *const SW = @"SW";
+NSString *const S = @"S";
+NSString *const SE = @"SE";
+NSString *const E = @"E";
+NSString *const NE = @"NE";
+NSString *const ERROR = @"ERROR";
+
+@implementation ShipIt
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
