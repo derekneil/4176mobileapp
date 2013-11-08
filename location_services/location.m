61e5e1da91dace9a3c0e2834f51f0c85d3c6635f
Derek Neil <derekrneil@gmail.com>
2013-11-08 10:44:20 -0400

    handle location file rename??

--- location_services/location.m
+++ location_services/location.m
@@ -1,87 +1,123 @@
-#import "location.h"
+#import "Location.h"
 #import "ShipFit.h"
 
-@implementation location
+@implementation Location
 
-// Custom initializer
-- (id) initWithReference: (ShipFit *)reference 
+// Error Handling
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
 {
-    self = [super init];
-    if ( self ){
-        _shipFit_ref = reference;
+#if 1
+    NSLog(@"%@", error);
+#endif
+    
+    
+    
+    if ( manager == self.gps_manager )
+    {
+        //
     }
-    return self;
+    
+    if ( manager == self.compass_manager )
+    {
+        //
+    }
+
+
 }
 
-// Initiazes a gps_manager with the accuracy and distance setting.
-// Controlling these settings is at the whim of the ShipFit.h class
-// Which contains the majority of functions that we need.
-// RETURNS -1 Location Services Not Available 0 Location Services Not Determined Yet
-// 1 If the GPS is Authorized and Running (ONE IS GOOD) (NEGATIVE ONE BAD)
-- (short int)run_GPS_withAccuracy: (CLLocationAccuracy)accuracy
-                andDistanceFilter: (CLLocationDistance)distance {
+
+
+- (unsigned short int)init_GPS
+{
+    short int returncode;
     
-    short int returncode = 0;
-    
-    // IF the user or application is not permitted to access the device's location.
-    if ( ( [CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted ) ||
-        ( [CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied ) )
+    switch ([CLLocationManager authorizationStatus])
     {
-        returncode = -1;
+        case 0:
+            returncode = kCLAuthorizationStatusNotDetermined;
+            break;
+        case 1:
+            returncode = kCLAuthorizationStatusRestricted;
+            break;
+        case 2:
+            returncode = kCLAuthorizationStatusDenied;
+            break;
+        case 3:
+            returncode = kCLAuthorizationStatusAuthorized;
+            break;
     }
     
-    // The application has full permission to access the location services
-    else if (  [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorized ){
-        self.gps_manager.distanceFilter = distance;
-        self.gps_manager.desiredAccuracy = accuracy;
-        returncode = 1;
-    }
-    
-    // Start Getting the Data
-    if( returncode > 0){
-        [ self.gps_manager startUpdatingLocation ];
+    if ( returncode == kCLAuthorizationStatusAuthorized )
+    {
+        self.gps_manager = [ [CLLocationManager alloc] init];
+        self.gps_manager.delegate = self;
     }
     
     return returncode;
 }
+
+- (short int)run_GPS_withAccuracy: (CLLocationAccuracy)accuracy
+                andDistanceFilter: (CLLocationDistance)distance
+{
+    short int returncode;
+    if ( [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorized )
+    {
+        self.gps_manager.distanceFilter = distance;
+        self.gps_manager.desiredAccuracy = accuracy;
+        [ self.gps_manager startUpdatingLocation ];
+        returncode = 1;
+    }
+    else
+    {
+        returncode = -1;
+    }
+    return returncode;
+}
+
 
 - (void)halt_GPS
 {
     [ self.gps_manager stopUpdatingLocation ];
 }
 
-- (short int)log_GPS
+- (void)log_latitude: (CLLocationDegrees)lat
+           longitude: (CLLocationDegrees)lon
 {
-    short int returncode = 1;
-    return returncode;
+    // to do
 }
 
-// iOS 5
-// Depracated in iOS 6 and 7. I want to test/run the app on the first GEN iPAD
+// iOS 5 location manager delegate method
 - (void)locationManager:(CLLocationManager *)manager 
     didUpdateToLocation:(CLLocation *)newLocation 
-           fromLocation:(CLLocation *)oldLocation{
+           fromLocation:(CLLocation *)oldLocation
+{
     
     // GPS MANAGER
-    if ( manager == self.gps_manager ){
+    if ( manager == self.gps_manager )
+    {
  
-        /* Make sure the time stamp is relevant. */ 
+        /* Make sure the time stamp is relevant */
         if ( ( [ [ NSDate date ]  timeIntervalSince1970 ] - [newLocation.timestamp timeIntervalSince1970 ] ) < 60 )
         { 
             /* Set the new lat and long values */
-            self.shipIt_ref.latitude = newLocation.coordinate.latitude;
-            self.shipIt_ref.longitude = newLocation.coordinate.longitude;
-
-
-            self.shipIt_ref.knots = ( (newLocation.speed) * 1.94384 );
+            self.shipFit_ref.latitude = newLocation.coordinate.latitude;
+            self.shipFit_ref.longitude = newLocation.coordinate.longitude;
+            
+            /* Log the new lat and long values */
+            [ self log_latitude:newLocation.coordinate.latitude
+                      longitude:newLocation.coordinate.longitude ];
+            
+            /* Set the new speed property */
+            self.shipFit_ref.knots = ( (newLocation.speed) * 1.94384 );
 #if 1
-            NSLog( @"LAT: %f\nLONG: %f\nKNOTS:%f" , self.shipIt_ref.latitude , self.shipIt_ref.longitude , self.shipIt_ref.knots);
+            NSLog( @"LAT: %f\nLONG: %f\nKNOTS:%f" , self.shipFit_ref.latitude , self.shipFit_ref.longitude , self.shipFit_ref.knots);
 #endif
         }
         else
         {
 #if 1
-            NSLog("Entry is over 1 minute old.....");
+            NSLog(@"Entry is over 1 minute old.....");
 #endif
         }
     }
@@ -90,65 +126,81 @@
 // iOS 6 & 7
 - (void)locationManager: (CLLocationManager *)manager 
      didUpdateLocations:(NSArray *)locations
-{    
+{
+    unsigned short int l;
+    
     if ( manager == self.gps_manager )
     {
        // Update the location
         CLLocation *current_location = [ locations lastObject ];
-        if ( ( [ [ NSDate date ]  timeIntervalSince1970 ] - [newLocation.timestamp timeIntervalSince1970 ] ) < 60 )
+        if ( ( [ [ NSDate date ]  timeIntervalSince1970 ] - [current_location.timestamp timeIntervalSince1970 ] ) < 60 )
         {
             /* Set the new lat/long */
-            self.shipIt_ref.latitude = current_location.coordinate.latitude;
-            self.shipIt_ref.longitude = current_location.coordinate.longitude;
-
-            self.shipIt_ref.speed_in_knots = ( current_location.speed * 1.94384 );
+            self.shipFit_ref.latitude = current_location.coordinate.latitude;
+            self.shipFit_ref.longitude = current_location.coordinate.longitude;
+            
+            /* Set the speed */
+            self.shipFit_ref.knots = ( current_location.speed * 1.94384 );
 #if 1
-            NSLog( @"LAT: %f\nLONG: %f\nKNOTS:%f" , self.shipIt_ref.latitude , self.shipIt_ref.longitude , self.shipIt_ref.knots);  
+            NSLog( @"LAT: %f\nLONG: %f\nKNOTS:%f" , self.shipFit_ref.latitude , self.shipFit_ref.longitude , self.shipFit_ref.knots);
 #endif     
         }
-        else{
+        else
+        {
 #if 1
-            NSLog("Entry is over 1 minute old......");
+            NSLog(@"Entry is over 1 minute old......");
 #endif
         }
-
-
-        // cycle through and log the elements. 
-        unsigned short int l = 0;
-        for ( l ; l < [locations count] ; l++)
+        
+        
+        for (l = 0 ; l < [locations count] ; l++)
         {
-            // log each element in the array. 
-            // maybe have to do some checking on the size of the logs
-            // also what is the most efficient way that we can
-            // how many reading do you think we need.
-            // how many does derek need to draw his line?
-            // it should be as spread out as possible. 
-            // don't ya think!! 
+            current_location = [locations objectAtIndex:l ];
+            [self log_latitude:current_location.coordinate.latitude
+                     longitude:current_location.coordinate.longitude ];
         }
-    } 
+
+    }
+    
+    
+    
+    
 }
+
+
+- (BOOL)init_compass
+{
+    if ( [ CLLocationManager headingAvailable ] )
+    {
+        self.compass_manager =  [ [CLLocationManager alloc] init ];
+        self.compass_manager.delegate = self;
+        return YES;
+    }
+    else
+    {
+        return NO;
+    }
+}
+
 
 // Returns
 // 1 on success
 // -1 on failure
-- (short int)run_compass
+- (BOOL)run_compass_withFilter: (CLLocationDegrees)compass_accuracy
 {
-    short int returncode;   
-    
-    if( [ CLLocationManager headingAvailable ] ){
-        self.compass_manager = [ [CLLocationManager alloc] init ];
+    if ( [ CLLocationManager headingAvailable ] )
+    {
+        self.compass_manager.headingFilter = compass_accuracy;
         [ self.compass_manager startUpdatingHeading ];
-        
-        returncode = 1;
+        return YES;
     }
-    else{
+    else
+    {
 #if 1
         NSLog(@"Compass Not Available");
-#endif 
-        returncode = -1;
+#endif
+        return NO;
     }
-    
-    return returncode;
 }
 
 - (void)halt_compass
@@ -160,15 +212,17 @@
 - (void)locationManager:(CLLocationManager *)manager 
        didUpdateHeading:(CLHeading *)newHeading
 {
-    if ( self.compass_manager == manager ){
-        
-        // Make sure the time stamp is relevant
-        if ( ( [ [ NSDate date ]  timeIntervalSince1970 ] - [newHeading.timestamp timeIntervalSince1970 ] ) < 5 ){
-            self.shipIt_ref.magnetic_north = newHeading.magneticHeading;
-            self.shipIt_ref.true_north = newHeading.trueHeading;
+    if ( self.compass_manager == manager && [CLLocationManager headingAvailable] )
+    {
+        if ( ( [ [ NSDate date ]  timeIntervalSince1970 ] - [newHeading.timestamp timeIntervalSince1970 ] ) < 5 )
+        {
+            self.shipFit_ref.magnetic_north = newHeading.magneticHeading;
+            self.shipFit_ref.true_north = newHeading.trueHeading;
+            [ self set_bearing ];
         }
-        else{
-            // to do
+        else
+        {
+            NSLog(@"Time stamp for the compass is stale. Take the according action");
         }
     }
 }
@@ -177,43 +231,58 @@
 // Sets the properties:
 // magnetic_north_bearing
 // true_north_bearing
-// based on the readings 
+// based on the readings of the compass.
 - (void)set_bearing
 {
-    if( self.shipIt_ref.true_north >= 337.5 || self.shipIt_ref.true_north <= 22.5 )self.shipIt_ref.true_north_bearing = N;
-    else if( self.shipIt_ref.true_north > 22.5 && self.shipIt_ref.true_north < 67.5 )self.shipIt_ref.true_north_bearing = NE;
-    else if( self.shipIt_ref.true_north >= 67.5 && self.shipIt_ref.true_north <= 112.5 )self.shipIt_ref.true_north_bearing = E;
-    else if( self.shipIt_ref.true_north > 112.5 && self.shipIt_ref.true_north < 157.5)self.shipIt_ref.true_north_bearing = SE;
-    else if( self.shipIt_ref.true_north >= 157.5 && self.shipIt_ref.true_north <= 202.5)self.shipIt_ref.true_north_bearing = S;
-    else if( self.shipIt_ref.true_north > 202.5 && self.shipIt_ref.true_north < 247.5 )self.shipIt_ref.true_north_bearing = SW;
-    else if( self.shipIt_ref.true_north >= 247.5 && self.shipIt_ref.true_north <= 292.5)self.shipIt_ref.true_north_bearing = W;
-    else if( self.shipIt_ref.true_north > 292.5 && self.shipIt_ref.true_north < 337.5)self.shipIt_ref.true_north_bearing = NW;
-    else if ( self.shipIt_ref.true_north == -1 )self.shipIt_ref.true_north_bearing = ERROR;
+    if( self.shipFit_ref.true_north >= 337.5 || self.shipFit_ref.true_north <= 22.5 )self.shipFit_ref.true_north_bearing = N;
+    else if( self.shipFit_ref.true_north > 22.5 && self.shipFit_ref.true_north < 67.5 )self.shipFit_ref.true_north_bearing = NE;
+    else if( self.shipFit_ref.true_north >= 67.5 && self.shipFit_ref.true_north <= 112.5 )self.shipFit_ref.true_north_bearing = E;
+    else if( self.shipFit_ref.true_north > 112.5 && self.shipFit_ref.true_north < 157.5)self.shipFit_ref.true_north_bearing = SE;
+    else if( self.shipFit_ref.true_north >= 157.5 && self.shipFit_ref.true_north <= 202.5)self.shipFit_ref.true_north_bearing = S;
+    else if( self.shipFit_ref.true_north > 202.5 && self.shipFit_ref.true_north < 247.5 )self.shipFit_ref.true_north_bearing = SW;
+    else if( self.shipFit_ref.true_north >= 247.5 && self.shipFit_ref.true_north <= 292.5)self.shipFit_ref.true_north_bearing = W;
+    else if( self.shipFit_ref.true_north > 292.5 && self.shipFit_ref.true_north < 337.5)self.shipFit_ref.true_north_bearing = NW;
+    else if ( self.shipFit_ref.true_north == -1 )self.shipFit_ref.true_north_bearing = ERROR;
     
-    if( self.shipIt_ref.magnetic_north >= 337.5 || self.shipIt_ref.magnetic_north <= 22.5 )self.shipIt_ref.magnetic_north_bearing = N;
-    else if( self.shipIt_ref.magnetic_north > 22.5 && self.shipIt_ref.magnetic_north < 67.5 )self.shipIt_ref.magnetic_north_bearing = NE;
-    else if( self.shipIt_ref.magnetic_north >= 67.5 && self.shipIt_ref.magnetic_north <= 112.5 )self.shipIt_ref.magnetic_north_bearing = E;
-    else if( self.shipIt_ref.magnetic_north > 112.5 && self.shipIt_ref.magnetic_north < 157.5)self.shipIt_ref.magnetic_north_bearing = SE;
-    else if( self.shipIt_ref.magnetic_north >= 157.5 && self.shipIt_ref.magnetic_north <= 202.5)self.shipIt_ref.magnetic_north_bearing = S;
-    else if( self.shipIt_ref.magnetic_north > 202.5 && self.shipIt_ref.magnetic_north < 247.5 )self.shipIt_ref.magnetic_north_bearing = SW;
-    else if( self.shipIt_ref.magnetic_north >= 247.5 && self.shipIt_ref.magnetic_north <= 292.5)self.shipIt_ref.magnetic_north_bearing = W;
-    else if( self.shipIt_ref.magnetic_north > 292.5 && self.shipIt_ref.magnetic_north < 337.5)self.shipIt_ref.magnetic_north_bearing = NW;
-    else if ( self.shipIt_ref.magnetic_north == -1 )self.shipIt_ref.magnetic_north_bearing = ERROR;
+    if( self.shipFit_ref.magnetic_north >= 337.5 || self.shipFit_ref.magnetic_north <= 22.5 )self.shipFit_ref.magnetic_north_bearing = N;
+    else if( self.shipFit_ref.magnetic_north > 22.5 && self.shipFit_ref.magnetic_north < 67.5 )self.shipFit_ref.magnetic_north_bearing = NE;
+    else if( self.shipFit_ref.magnetic_north >= 67.5 && self.shipFit_ref.magnetic_north <= 112.5 )self.shipFit_ref.magnetic_north_bearing = E;
+    else if( self.shipFit_ref.magnetic_north > 112.5 && self.shipFit_ref.magnetic_north < 157.5)self.shipFit_ref.magnetic_north_bearing = SE;
+    else if( self.shipFit_ref.magnetic_north >= 157.5 && self.shipFit_ref.magnetic_north <= 202.5)self.shipFit_ref.magnetic_north_bearing = S;
+    else if( self.shipFit_ref.magnetic_north > 202.5 && self.shipFit_ref.magnetic_north < 247.5 )self.shipFit_ref.magnetic_north_bearing = SW;
+    else if( self.shipFit_ref.magnetic_north >= 247.5 && self.shipFit_ref.magnetic_north <= 292.5)self.shipFit_ref.magnetic_north_bearing = W;
+    else if( self.shipFit_ref.magnetic_north > 292.5 && self.shipFit_ref.magnetic_north < 337.5)self.shipFit_ref.magnetic_north_bearing = NW;
+    else if ( self.shipFit_ref.magnetic_north == -1 )self.shipFit_ref.magnetic_north_bearing = ERROR;
 }
 
 
+- (id) initWithReference: (ShipFit *)reference
+{
+    self = [super init];
+    if ( self ){
+        _shipFit_ref = reference;
+    }
+    return self;
+}
+
+@end
+
+
+// Initiazes a gps_manager with the accuracy and distance setting.
+// Controlling these settings is at the whim of the ShipFit.h class
+// Which contains the majority of functions that we need.
+// RETURNS -1 Location Services Not Available 0 Location Services Not Determined Yet
+// 1 If the GPS is Authorized and Running (ONE IS GOOD) (NEGATIVE ONE BAD)
 // When the location manager calls this method an error was encountered
 // when trying to get the location or heading data. If the service was
 // unable to get a location error right away it reports a kCLErrorLocationUnkown
 // error and keeps trying. This can be ingnored. However, a kCLErrorDenied is
 // received if the User denies LS. A kCLErrorHeadingFailure is due to interference.
-- (void)locationManager:(CLLocationManager *)manager 
-       didFailWithError:(NSError *)error{
-    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied){
-        NSLog(@"User has denied location services");
-    } else {
-        NSLog(@"Location manager did fail with error: %@", error.localizedFailureReason);
-    }
-}
 
-@end
+// log each element in the array.
+// maybe have to do some checking on the size of the logs
+// also what is the most efficient way that we can
+// how many reading do you think we need.
+// how many does derek need to draw his line?
+// it should be as spread out as possible.
+// don't ya think!!
