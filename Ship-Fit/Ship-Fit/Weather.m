2b6efd340f0eb37c608ed5dce2a7f5247d18c724
Ben <jammin.irving@gmail.com>
2013-11-14 15:19:01 -0400

    Weather Skeleton Added.

---
+++ Ship-Fit/Ship-Fit/Weather.m
@@ -0,0 +1,13 @@
+//
+//  Weather.m
+//  Ship-Fit
+//
+//  Created by Ben Irving on 11/14/13.
+//  Copyright (c) 2013 ship-fit. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+@end
