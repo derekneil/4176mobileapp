b37d976c2f2c3f69484255b02bf0378988c46dad
Ben <jammin.irving@gmail.com>
2013-11-19 21:01:29 -0400

    adding Jander's ViewController for Ipad. This continas the table views for the weather, we still need a reference in the app delegate, derek.

---
+++ Ship-Fit/Ship-Fit/TableCell.m
@@ -0,0 +1,13 @@
+//
+//  TableCell.m
+//  Ship-Fit
+//
+//  Created by Ben Irving on 11/19/13.
+//  Copyright (c) 2013 ship-fit. All rights reserved.
+//
+
+#import "TableCell.h"
+
+@implementation TableCell
+
+@end
