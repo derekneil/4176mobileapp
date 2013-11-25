37d2fb3902c3336ca3624ab6292097c1d6de64df
Derek Neil <derekrneil@gmail.com>
2013-11-24 20:27:52 -0400

    rest of mapBox framework

---
+++ Ship-Fit/Ship-Fit/WeatherDelegate.h
@@ -0,0 +1,11 @@
+//
+//#import <Foundation/Foundation.h>
+//
+//@protocol WeatherDelegate
+//
+//- (void)receivedJSON:(NSData *)objectNotation;
+//- (void)fetchingWeatherFailedWithError:(NSError *)error;
+//
+//@end
+// I want to remove all of this......
+// Don't want to mess up the git repo...
